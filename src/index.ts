13d72420766559708a6a50212bc699c2edff056b
1REZ3 <benitopedro21@outlook.com>
2021-08-20 20:26:56 -0300

    Formatting

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,14 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import {Request, Response} from "express";
 import {Routes} from "./routes";
-import {User} from "./entity/User";
 
 createConnection().then(async connection => {
 
-    // create express app
+    // Criar aplicativo express
     const app = express();
     app.use(bodyParser.json());
 
-    // register express routes from defined application routes
+    // Registrar rotas apartir das definidas no aplicativo
     Routes.forEach(route => {
         (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
             const result = (new (route.controller as any))[route.action](req, res, next);
@@ -25,10 +24,6 @@ createConnection().then(async connection => {
         });
     });
 
-    // setup express app here
-    
-
-    // start express server
     app.listen(3000);
 
     console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
